6bdc13256c5db8ec5645508e2da40f8604d1a044
Xaviera Arias <xaviera.arias.a@gmail.com>
2021-06-14 23:20:44 -0400

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:calistenia_app/pages/login.dart';
+import 'package:calistenia_app/screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
